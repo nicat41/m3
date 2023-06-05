847395e4ed03265cb257b8cff8dfe8c20e5f241b
nicat41 <nicadhesimov871@gmail.com>
2023-06-05 20:11:19 +0400

    buttonlari click ederken valutalarini deyisdirmek,

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
 const from = "RUB";
 const to = "USD";
 
 const inputLeft = document.querySelector(".i-have .input-block input");
 const inputRight = document.querySelector(".want-to-get .second-input-block input");
 inputLeft.value = 5000;
-inputRight.value = 67.6685;
+// inputRight.value = 67.6685;
+
+const firstBlock = document.querySelector(".input-block");
+const secondBlock = document.querySelector(".second-input-block");
 
 const getCurrencyRate = async (base, symbol) => {
   const response = await fetch(
@@ -16,32 +18,47 @@ const getCurrencyRate = async (base, symbol) => {
   return data;
 };
 
-const firstBlock = document.querySelector(".input-block");
+const leftPElement = document.createElement("p");
+const rightPElement = document.createElement("p");
 
-getCurrencyRate(from, to).then((res) => {
-  const pElement = document.createElement("p");
-  const rateSymbol = Object.keys(res.rates)[0];
-  const rateValue = Object.values(res.rates)[0];
-  console.log(rateValue);
-  pElement.innerText = `1 ${res.base} = ${rateValue} ${rateSymbol}`;
+const updateCurrencies = (from, to) => {
+  getCurrencyRate(from, to).then((res) => {
+    // const pElement = document.createElement("p");
+    const rateSymbol = Object.keys(res.rates)[0];
+    const rateValue = Object.values(res.rates)[0];
+    console.log(rateValue);
+    leftPElement.innerText = `1 ${res.base} = ${rateValue} ${rateSymbol}`;
+    rightPElement.innerText = `1 ${rateSymbol} = ${(1 / rateValue).toFixed(
+      4
+    )} ${res.base}`;
 
-  firstBlock.append(pElement);
+    firstBlock.append(leftPElement);
+    secondBlock.append(rightPElement);
+  });
+};
+
+const leftSideBtns = document.querySelectorAll(".left-currencies button");
+const rightSideBtns = document.querySelectorAll(".right-currencies button");
+
+leftSideBtns.forEach((btn) => {
+  btn.addEventListener("click", changeLeftCurrency);
 });
 
-const secondBlock = document.querySelector(".second-input-block");
+function changeLeftCurrency(e) {
+  leftSideBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-getCurrencyRate(to, from).then((res) => {
-  const pElement = document.createElement("p");
-  const rateSymbol = Object.keys(res.rates)[0];
-  const rateValue = Object.values(res.rates)[0];
-  console.log(rateValue);
-  pElement.innerText = `1 ${res.base} = ${rateValue} ${rateSymbol}`;
+  e.target.classList.add("active");
+  let from = document.querySelector(".left-currencies .active").innerText;
+  let to = document.querySelector(".right-currencies .active").innerText;
+  updateCurrencies(from, to);
+}
 
-  secondBlock.append(pElement);
-});
-
-inputLeft.addEventListener("keyup", (e) => {
+inputLeft.addEventListener("input", (e) => {
   let calculatedValue;
+  let from = document.querySelector(".left-currencies .active").innerText;
+  let to = document.querySelector(".right-currencies .active").innerText;
   getCurrencyRate(from, to).then((res) => {
     const rateValue = Object.values(res.rates)[0];
     calculatedValue = e.target.value * rateValue;
@@ -49,14 +66,17 @@ inputLeft.addEventListener("keyup", (e) => {
     console.log(calculatedValue);
   });
 });
-inputRight.addEventListener("keyup", (e) => {
+
+inputRight.addEventListener("input", (e) => {
   let calculatedValue;
-  getCurrencyRate(to, from).then((res) => {
+  let from = document.querySelector(".left-currencies .active").innerText;
+  let to = document.querySelector(".right-currencies .active").innerText;
+  getCurrencyRate(from, to).then((res) => {
     const rateValue = Object.values(res.rates)[0];
-    calculatedValue = e.target.value * rateValue;
-    inputLeft.value = calculatedValue;
+    calculatedValue = e.target.value * (1 / rateValue).toFixed(4);
+    inputRight.value = calculatedValue;
     console.log(calculatedValue);
   });
 });
 
-
+updateCurrencies(from, to);
